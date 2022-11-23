b997773d8934fcf4d93d279412657bfd1bb929af
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-11-23 17:01:54 -0500

    Complete Lecture 8/14 for Africa App
    Let's develop a complex Map with custom Annotations in SwiftUI - Part 1

--- 04-Africa-App/Africa/Africa/App/MapView.swift
+++ 04-Africa-App/Africa/Africa/App/MapView.swift
@@ -5,14 +5,40 @@
 //  Created by Matt X on 11/9/22.
 //
 
+import MapKit
 import SwiftUI
 
 struct MapView: View {
     // MARK: - PROPERTIES
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(
+            latitude: 6.600286,
+            longitude: 16.4377599
+        )
+        var mapZoomLevel = MKCoordinateSpan(
+            latitudeDelta: 70.0,
+            longitudeDelta: 70.0
+        )
+        var mapRegion = MKCoordinateRegion(
+            center: mapCoordinates,
+            span: mapZoomLevel
+        )
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
     
     // MARK: - BODY
     var body: some View {
-        Text("Map")
+        Map(coordinateRegion: $region, annotationItems: locations) { location in
+            MapAnnotation(coordinate: location.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            }
+        }
     }
 }
 
