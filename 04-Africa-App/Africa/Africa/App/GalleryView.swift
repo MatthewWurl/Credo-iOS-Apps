fbff2d2cee98274c4e8af47455f9a193418783cd
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-11-26 22:38:29 -0500

    Complete Lecture 11/14 for Africa App
    Let's create a photo gallery with Grid Layout using SwiftUI

--- 04-Africa-App/Africa/Africa/App/GalleryView.swift
+++ 04-Africa-App/Africa/Africa/App/GalleryView.swift
@@ -9,14 +9,66 @@ import SwiftUI
 
 struct GalleryView: View {
     // MARK: - PROPERTIES
+    @State private var selectedAnimal: String = "lion"
+    @State private var gridLayout = [GridItem(.flexible())]
+    @State private var gridColumns = 3.0
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
     
     // MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack(alignment: .center, spacing: 30) {
+                // MARK: - IMAGE
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(.white, lineWidth: 8)
+                    )
+                
+                // MARK: - SLIDER
+                Slider(value: $gridColumns, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumns) { _ in
+                        gridSwitch()
+                    }
+                
+                // MARK: - GRID
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { animal in
+                        Image(animal.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(
+                                Circle()
+                                    .stroke(.white, lineWidth: 1)
+                            )
+                            .onTapGesture {
+                                selectedAnimal = animal.image
+                                
+                                haptics.impactOccurred()
+                            }
+                    }
+                }
+                .animation(.easeIn)
+                .onAppear {
+                    gridSwitch()
+                }
+            }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
+    }
+    
+    // MARK: - FUNCTIONS
+    func gridSwitch() {
+        gridLayout = Array(repeating: GridItem(.flexible()), count: Int(gridColumns))
     }
 }
 
