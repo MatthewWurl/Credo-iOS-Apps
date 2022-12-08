2659180c13bd9da03f86e5ae4b962182e11118eb
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-12-08 13:38:48 -0500

    Complete Lecture 12/14 for Africa App
    Let's develop a complex Grid Layout system with four Toolbar Buttons

--- 04-Africa-App/Africa/Africa/App/ContentView.swift
+++ 04-Africa-App/Africa/Africa/App/ContentView.swift
@@ -9,29 +9,103 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - PROPERTIES
+    @State private var gridColumn = 1
+    @State private var gridLayout: [GridItem] = [ GridItem(.flexible()) ]
+    @State private var isGridViewActive = false
+    @State private var toolbarIcon = "square.grid.2x2"
+    
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
     
     // MARK: - BODY
     var body: some View {
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(
-                        EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-                    )
-                
-                ForEach(animals) { animal in
-                    NavigationLink {
-                        AnimalDetailView(animal: animal)
-                    } label: {
-                        AnimalListItemView(animal: animal)
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(
+                                EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+                            )
+                        
+                        ForEach(animals) { animal in
+                            NavigationLink {
+                                AnimalDetailView(animal: animal)
+                            } label: {
+                                AnimalListItemView(animal: animal)
+                            }
+                        }
+                    }
+                    .listStyle(.plain)
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { animal in
+                                NavigationLink {
+                                    AnimalDetailView(animal: animal)
+                                } label: {
+                                    AnimalGridItemView(animal: animal)
+                                }
+                            }
+                        }
+                        .padding(10)
+                        .animation(.easeIn)
                     }
                 }
             }
-            .listStyle(.plain)
             .navigationTitle("Africa")
             .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        // LIST
+                        Button {
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        } label: {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(
+                                    isGridViewActive ? .primary : .accentColor
+                                )
+                        }
+                        
+                        // GRID
+                        Button {
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                            gridSwitch()
+                        } label: {
+                            Image(systemName: toolbarIcon)
+                                .font(.title2)
+                                .foregroundColor(
+                                    isGridViewActive ? .accentColor : .primary
+                                )
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // MARK: - FUNCTIONS
+    func gridSwitch() {
+        gridLayout = Array(
+            repeating: .init(.flexible()),
+            count: gridLayout.count % 3 + 1
+        )
+        gridColumn = gridLayout.count
+        
+        switch gridColumn {
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
         }
     }
 }
